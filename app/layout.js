40b11084fbf9789d751ddcb8690b0a38c416f993
heyyuday <heyyuday@gmail.com>
2026-04-03 04:32:04 +0000

    Fixed 404 layout error

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,21 @@
+import './globals.css'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
 export const metadata = {
   title: 'Nahid Ashraf Uday | Bangladeshi Author | Best Sarcastic Humor Writer | Bengali Books',
-  description: 'Nahid Ashraf Uday - Bangladeshi author from Brahmanbaria. Published "Hawai Mithai" (4.56★) and "Paap Ebong Punno" at 2023 Ekushey Book Fair. Best sarcastic humor writer blending Bengali satire with social commentary. Featured in Jugantor, Protidiner Bangladesh, Earki. Master\'s student at University of Adelaide.',
-  keywords: 'Nahid Ashraf Uday, Bangladeshi author, Bengali writer, Brahmanbaria author, Hawai Mithai, Paap Ebong Punno, best sarcastic writer Bangladesh, Bengali humor books, Ekushey Book Fair 2023, Humayun Ahmed, Bangladeshi literature, content writer Bangladesh, Bengali satire, Rokomari books, Boibazar, Goodreads author, University of Adelaide',
-  authors: [{ name: 'Nahid Ashraf Uday' }],
-  openGraph: {
-    title: 'Nahid Ashraf Uday | Best Sarcastic Humor Writer Bangladesh',
-    description: 'Published Bengali author of "Hawai Mithai" (4.56★) and "Paap Ebong Punno". Blending humor, satire, and social commentary in Bengali literature.',
-    url: 'https://nahidashrafuday.com',
-    siteName: 'Nahid Ashraf Uday',
-    locale: 'en_US',
-    type: 'website',
-  },
+  description: 'Nahid Ashraf Uday - Bangladeshi author from Brahmanbaria. Published "Hawai Mithai" (4.56★) and "Paap Ebong Punno" at 2023 Ekushey Book Fair. Best sarcastic humor writer blending Bengali satire with social commentary.',
+  keywords: 'Nahid Ashraf Uday, Bangladeshi author, Bengali writer, Brahmanbaria author, Hawai Mithai, Paap Ebong Punno, best sarcastic writer Bangladesh, Bengali humor books',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <Header />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  )
 }
