03a0a100c0f8bfb244eaf440acf197ac585a4441
heyyuday <heyyuday@gmail.com>
2026-04-03 10:47:03 +0000

    Fixed all layout files

---
+++ app/about/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: 'About Nahid Ashraf Uday | Bengali Author Bangladesh',
+  description: 'Bangladeshi author, copywriter, and Facebook marketing expert from Brahmanbaria.',
+}
+
+export default function AboutLayout({ children }) {
+  return children
+}
---
+++ app/blog/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: 'Blog | Nahid Ashraf Uday',
+  description: 'Writing tips, marketing insights, and thoughts on Bengali literature.',
+}
+
+export default function BlogLayout({ children }) {
+  return children
+}
